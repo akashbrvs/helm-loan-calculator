1975b6831e984c3613a07453d32a1126378553e8
akashbrvs <131256775+akashbrvs@users.noreply.github.com>
2023-04-26 09:13:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 ACTION=$1
 CHART=$2
+UPSTREAM="build-loan-calculator-app"
+JENKINS_WORKSPACE="/var/lib/jenkins/workspace"
+TAG=$(cat /var/lib/jenkins/workspace/$UPSTREAM/versionfile | xargs echo -n)
 STATUS=$(helm status $CHART 2> /dev/null | grep 'STATUS' | cut -f2 -d":" |xargs)
 if [ "$ACTION" = "install" ]; then
     echo -e "\nchecking Helm chart $CHART \n"
        if [ "$STATUS" = "deployed" ]; then
           echo -e "\nHelm chart $CHART is already installed \n"
           echo -e "\nUpgrading Helm chart $CHART\n"
-          helm upgrade $CHART ./helm-chart
+          helm upgrade $CHART ./helm-chart --set image.tag=$TAG
        else
           echo -e "\nHelm chart $CHART is not installed\n "
           echo -e "\nInstalling Helm chart $CHART\n"
-          helm install $CHART ./helm-chart
+          helm install $CHART ./helm-chart --set image.tag=$TAG
        fi
 elif [ "$ACTION" = "uninstall" ]; then
      if [ "$STATUS" = "deployed" ]; then
@@ -24,4 +27,3 @@ elif [ "$ACTION" = "uninstall" ]; then
 else
       echo -e "\nWrong action please select install or uninstall\n"    
 fi
-
